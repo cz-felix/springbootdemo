c1bc08ab043a028cf1c5bc03b592ee72f28d6b0d
HKHF\chenzhi <chenzhi@huntkey.net>
2018-01-29 08:41:38 +0800

    流程设计保存关闭弹出框

--- src/main/java/com/demo/controller/activiti/ProcessDefController.java
+++ src/main/java/com/demo/controller/activiti/ProcessDefController.java
@@ -10,9 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
